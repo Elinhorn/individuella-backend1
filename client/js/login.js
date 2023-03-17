9587211bdd71138395b5ab52ee4a5ed3ee43d022
elinhorn <elin.horn@hyperisland.se>
2023-03-17 19:21:44 +0100

    :tada:added connection to frontend

---
+++ client/js/login.js
@@ -0,0 +1,21 @@
+const nameInput = document.querySelector('#nameInput')
+const passInput = document.querySelector('#passInput')
+const from = document.querySelector('form')
+
+from.addEventListener('submit', async (e) => {
+    e.preventDefault()
+
+    const username = nameInput.value
+    const password = passInput.value
+
+    const res = await fetch('http://localhost:1010/auth/login', {
+        method: 'POST',
+        body: JSON.stringify({username, password}),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        credentials:'include'
+
+    })
+    console.log(res)
+})
